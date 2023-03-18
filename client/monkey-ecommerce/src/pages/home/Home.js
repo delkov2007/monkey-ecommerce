872cb3687134b602e49d4c8f9eee42eb4f5000f2
Dimitar Elkov <delkov@sapsservices.com>
2023-03-19 00:28:15 +0200

    Remove old rxjs store

--- client/monkey-ecommerce/src/pages/home/Home.js
+++ client/monkey-ecommerce/src/pages/home/Home.js
@@ -1,21 +1,7 @@
-import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import userStore from "../../stores/user.store";
 
 const Home = () => {
     const navigate = useNavigate();
-    const [user, setUser] = useState(userStore.initialState);
-
-    useEffect(() => {
-        const subscription = userStore.subscribe(setUser);
-        return subscription.unsubscribe();
-    }, []);
-
-    useEffect(() => {
-        debugger;
-        const canActivate = user;
-    }, [user]);
-
     return (
         <h1>Home page</h1>
     );
